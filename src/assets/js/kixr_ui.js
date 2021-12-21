86911129fd4e597c64f3946d97719f180a37189a
sekhar <sekhar.kotakonda@kiksarvr.com>
2021-12-21 20:42:39 +0530

    initial commit

---
+++ src/assets/js/kixr_ui.js
@@ -0,0 +1,26 @@
+// let img_path = "assets/Bitmap/png/Bitmap Copy 7.png";
+// let Description = ""
+
+// var sourcemodel=[1,2,3,4];
+
+// function setDom() {
+  
+//         $('.imgAndData').attr('src', 'assets/Bitmap/png/Bitmap Copy 7.png')
+    
+// }
+
+// $(document).ready(function () {
+//     debugger
+//     setDom();
+// })
+
+// function appendData(){
+//     $('#doctorsData ul').append(`
+//     <li  class="${sourcemodel.length > 0 && index == 0 ? 'favorite' : ''}"> 
+       
+//         <div  id="` + item.skuId + `" target="_self">
+//             <img data-u="image" src="`+ item.fishEyeImage + `">
+//         </div>
+       
+//     </li>`)
+// }
